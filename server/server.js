18355063af99a47c5791d49637531502dde28284
hikartik <kartik.200904@gmail.com>
2025-06-06 01:12:34 +0530

    making production changes

--- server/server.js
+++ server/server.js
@@ -57,5 +57,9 @@ app.use("/api/messages", messageRouter);
 // Connect to MongoDB and start listening
 await connectDB();
 
+if(process.env.NODE_ENV!="production"){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Server is running on port:", PORT));
+}
+//export server for vercel
+export default server
